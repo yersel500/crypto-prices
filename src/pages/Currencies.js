166514a7c52e49a4dd3cfeaba3bcb7a7cfe4bb94
Yersel Hurtado <yersel500@gmail.com>
2022-04-11 15:26:33 -0500

    Fetch data from API

--- src/pages/Currencies.js
+++ src/pages/Currencies.js
@@ -1,7 +1,18 @@
-const Currencies = () => (
-  <div>
-    Currencies
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { LoadData } from '../redux/CryptoReducer';
+
+const Currencies = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(LoadData());
+  }, []);
+
+  return (
+    <div>
+      Currencies
+    </div>
+  );
+};
 
 export default Currencies;
--- src/redux/CryptoReducer.js
+++ src/redux/CryptoReducer.js
@@ -1,11 +1,18 @@
 //  Actions
 const FETCH_DATA = 'pages/Currencies/FETCH_DATA';
+const URL = 'https://api.coincap.io/v2/assets';
 
 // Action Creator
 export const FetchData = (data) => ({
   type: FETCH_DATA,
   payload: data,
 });
+
+export const LoadData = () => async () => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  console.log(data);
+};
 
 export const CryptoReducer = (state, action) => {
   switch (action.type) {
